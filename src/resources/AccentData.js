4ddc7b35b22c805955e25a15759964f3ada1c325
emr404 <roufaym@gmail.com>
2020-09-04 15:29:21 +0100

    Added accents file

---
+++ src/resources/AccentData.js
@@ -0,0 +1,17 @@
+const accent = {
+    easy: ["Spanish", "Italian", "German", "Indian", "Cockney"],
+
+    medium: ["Russian", "Dutch", "Birmingham", "Manchester"],
+
+    hard: [
+        "Irish",
+        "New York",
+        "New Jersey",
+        "Ghanian",
+        "Nigerian",
+        "South African",
+        "Jamaican",
+        "French",
+    ],
+};
+export default accent;
